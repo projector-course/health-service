f5083f10948c0505d32f179c71b80319a55b820b
mykhailo-vaskivnyuk <m.vaskivnyuk@gmail.com>
2021-11-07 16:44:54 +0200

    feat: create health service structure

---
+++ src/config.js
@@ -0,0 +1,10 @@
+const HOST = '127.0.0.1';
+const PORT = 3004;
+const BASE_URL = `http://${HOST}:${PORT}`;
+const SERVICE_NAME = 'HEALTH SERVICE';
+
+module.exports = {
+  PORT,
+  BASE_URL,
+  SERVICE_NAME,
+};
